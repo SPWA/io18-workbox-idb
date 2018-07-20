c70f05c62a7072da3b401ce0e4d80fec0e4e0189
Satya Kresna <satyakresna6295@gmail.com>
2018-07-21 07:16:59 +0800

    Get started finished

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,7 @@ limitations under the License.
 const gulp = require('gulp');
 const del = require('del');
 const runSequence = require('run-sequence');
+const workboxBuild = require('workbox-build')
 
 // this task removes old files
 gulp.task('clean', () => del(['.tmp', 'build/*', '!build/.git'], {dot: true}));
@@ -33,6 +34,7 @@ gulp.task('copy', () =>
 gulp.task('default', ['clean'], cb => {
   runSequence(
     'copy',
+    'service-worker',
     cb
   );
 });
@@ -40,4 +42,23 @@ gulp.task('default', ['clean'], cb => {
 // this task watches our "app" files & rebuilds whenever they change
 gulp.task('watch', function() {
   gulp.watch('app/**/*', ['default']);
+});
+
+// Generate the list of files to precache with Workbox
+gulp.task('service-worker', () => {
+  return workboxBuild.injectManifest({
+    "globDirectory": "build/",
+    "globPatterns": [
+      "style/main.css",
+      "index.html",
+      "js/idb-promised.js",
+      "js/main.js",
+      "images/**/*.*",
+      "manifest.json",
+    ],
+    "swDest": "build/sw.js",
+    "swSrc": "app/sw.js",
+  }).catch(err => {
+    console.log('[ERROR]: ' + err);
+  });
 });
